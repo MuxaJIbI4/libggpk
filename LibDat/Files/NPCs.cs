2c7a2c6b0da871f41ffa860cc31767407e4e1712
MadBrahmin <madbrahmin@gmail.com>
2013-09-29 17:10:31 -0400

    Updated for 0.11.6

--- LibDat/Files/NPCs.cs
+++ LibDat/Files/NPCs.cs
@@ -8,74 +8,67 @@ namespace LibDat.Files
 		public int Index0 { get; set; }
 		[UserStringIndex]
 		public int Index1 { get; set; }
-		[StringIndex]
-		public int Index2 { get; set; }
-		[StringIndex]
-		public int Index3 { get; set; }
-		public int Unknown0 { get; set; }
+		public int Unkonwn0 { get; set; }
 		public int Unknown1 { get; set; }
-		[StringIndex]
-		public int Index4 { get; set; }
 		public int Unknown2 { get; set; }
-		public int Index5 { get; set; }
 		public int Unknown3 { get; set; }
 		public int Unknown4 { get; set; }
-		[StringIndex]
-		public int Index6 { get; set; }
 		public int Unknown5 { get; set; }
 		public int Unknown6 { get; set; }
 		public int Unknown7 { get; set; }
-		public int Index7 { get; set; }
 		public int Unknown8 { get; set; }
 		public int Unknown9 { get; set; }
+		public int Unknown10 { get; set; }
+		public int Unknown11 { get; set; }
+		public int Unknown12 { get; set; }
+		public int Unknown13 { get; set; }
+		public int Unknown14 { get; set; }
 
 		public NPCs(BinaryReader inStream)
 		{
 			Index0 = inStream.ReadInt32();
 			Index1 = inStream.ReadInt32();
-			Index2 = inStream.ReadInt32();
-			Index3 = inStream.ReadInt32();
-			Unknown0 = inStream.ReadInt32();
+			Unkonwn0 = inStream.ReadInt32();
 			Unknown1 = inStream.ReadInt32();
-			Index4 = inStream.ReadInt32();
 			Unknown2 = inStream.ReadInt32();
-			Index5 = inStream.ReadInt32();
 			Unknown3 = inStream.ReadInt32();
 			Unknown4 = inStream.ReadInt32();
-			Index6 = inStream.ReadInt32();
 			Unknown5 = inStream.ReadInt32();
 			Unknown6 = inStream.ReadInt32();
 			Unknown7 = inStream.ReadInt32();
-			Index7 = inStream.ReadInt32();
 			Unknown8 = inStream.ReadInt32();
 			Unknown9 = inStream.ReadInt32();
+			Unknown10 = inStream.ReadInt32();
+			Unknown11 = inStream.ReadInt32();
+			Unknown12 = inStream.ReadInt32();
+			Unknown13 = inStream.ReadInt32();
+			Unknown14 = inStream.ReadInt32();
 		}
 
 		public override void Save(BinaryWriter outStream)
 		{
 			outStream.Write(Index0);
 			outStream.Write(Index1);
-			outStream.Write(Index2);
-			outStream.Write(Index3);
-			outStream.Write(Unknown0);
+			outStream.Write(Unkonwn0);
 			outStream.Write(Unknown1);
-			outStream.Write(Index4);
 			outStream.Write(Unknown2);
-			outStream.Write(Index5);
 			outStream.Write(Unknown3);
 			outStream.Write(Unknown4);
-			outStream.Write(Index6);
 			outStream.Write(Unknown5);
 			outStream.Write(Unknown6);
 			outStream.Write(Unknown7);
-			outStream.Write(Index7);
 			outStream.Write(Unknown8);
 			outStream.Write(Unknown9);
+			outStream.Write(Unknown10);
+			outStream.Write(Unknown11);
+			outStream.Write(Unknown12);
+			outStream.Write(Unknown13);
+			outStream.Write(Unknown14);
 		}
 
 		public override int GetSize()
 		{
-			return 0x48;
+			return 0x44;
 		}
 	}
 }
--- LibDat/Files/QuestStates.cs
+++ LibDat/Files/QuestStates.cs
@@ -8,7 +8,6 @@ namespace LibDat.Files
 		public int Unknown1 { get; set; }
 		public int Unknown2 { get; set; }
 		public int Unknown3 { get; set; }
-		[DataIndex]
 		public int Data0 { get; set; }
 		public int Unknown4 { get; set; }
 		public int Unknown5 { get; set; }
@@ -17,7 +16,6 @@ namespace LibDat.Files
 		public int Unknown6 { get; set; }
 		public bool Flag0 { get; set; }
 		public int Unknown7 { get; set; }
-		[DataIndex]
 		public int Data1 { get; set; }
 		public int Unknown9 { get; set; }
 
