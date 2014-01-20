b95aaeeb6e2bf54154a3298ee561e2c0c1259f8e
Chuan-Hsing Shen <chuanhsing@gmail.com>
2014-01-20 14:09:14 +0800

    Updated for 1.0.5b

--- LibDat/Files/Realms.cs
+++ LibDat/Files/Realms.cs
@@ -8,29 +8,39 @@ namespace LibDat.Files
 		public int Id { get; set; }
 		[UserStringIndex]
 		public int Name { get; set; }
-		[StringIndex]
+		public int ServerLength { get; set; }
+		[DataIndex]
 		public int Server { get; set; }
 		public bool Flag0 { get; set; }
+		public int ServerLength2 { get; set; }
+		[DataIndex]
+		public int Server2 { get; set; }
 
 		public Realms(BinaryReader inStream)
 		{
 			Id = inStream.ReadInt32();
 			Name = inStream.ReadInt32();
+			ServerLength = inStream.ReadInt32();
 			Server = inStream.ReadInt32();
 			Flag0 = inStream.ReadBoolean();
+			ServerLength2 = inStream.ReadInt32();
+			Server2 = inStream.ReadInt32();
 		}
 
 		public override void Save(BinaryWriter outStream)
 		{
 			outStream.Write(Id);
 			outStream.Write(Name);
+			outStream.Write(ServerLength);
 			outStream.Write(Server);
 			outStream.Write(Flag0);
+			outStream.Write(ServerLength2);
+			outStream.Write(Server2);
 		}
 
 		public override int GetSize()
 		{
-			return 0xD;
+			return 0x1D;
 		}
 	}
 }
