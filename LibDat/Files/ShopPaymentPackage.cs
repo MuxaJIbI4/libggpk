1505a938134064c9c26485e3ae7184e6241f5108
chuanhsing <chuanhsing@gmail.com>
2014-08-08 14:40:39 +0800

    Updated for v1.1.5e

--- LibDat/Files/ShopPaymentPackage.cs
+++ LibDat/Files/ShopPaymentPackage.cs
@@ -14,6 +14,10 @@ namespace LibDat.Files
 		public int Unknown4 { get; set; }
 		public int Unknown5 { get; set; }
 		public int Unknown6 { get; set; }
+		public int Unknown7 { get; set; }
+		public int Unknown8 { get; set; }
+		public int Unknown9 { get; set; }
+		public short Unknown10 { get; set; }
 
 		public ShopPaymentPackage(BinaryReader inStream)
 		{
@@ -25,6 +29,10 @@ namespace LibDat.Files
 			Unknown4 = inStream.ReadInt32();
 			Unknown5 = inStream.ReadInt32();
 			Unknown6 = inStream.ReadInt32();
+			Unknown7 = inStream.ReadInt32();
+			Unknown8 = inStream.ReadInt32();
+			Unknown9 = inStream.ReadInt32();
+			Unknown10 = inStream.ReadInt16();
 		}
 
 		public override void Save(BinaryWriter outStream)
@@ -37,11 +45,15 @@ namespace LibDat.Files
 			outStream.Write(Unknown4);
 			outStream.Write(Unknown5);
 			outStream.Write(Unknown6);
+			outStream.Write(Unknown7);
+			outStream.Write(Unknown8);
+			outStream.Write(Unknown9);
+			outStream.Write(Unknown10);
 		}
 
 		public override int GetSize()
 		{
-			return 0x1D;
+			return 0x2B;
 		}
 	}
 }
