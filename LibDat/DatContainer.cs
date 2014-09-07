2ec33ed800435b2836b1ada7d451ddb1476075f7
MuxaJIbI4 <samonon@gmail.com>
2014-09-08 01:07:57 +0400

    added some classes and changed behaviour:
    1) rename file RecordContainer to DatContainer as it should be
    2) BaseRecord > Record
    3) DatFactory > RecordFactory. will be removed later
    4) 2 new classes:
    - RecordInfo: contains info about single record in *.dat file
    - RecordFieldInfo: contains information about single record fields
    5) changed behaviour or Record (BaseRecord):
    - not it saves and writes data according to information in RecordInfoa and RecordFieldInfo

--- LibDat/DatContainer.cs
+++ LibDat/DatContainer.cs
@@ -5,6 +5,8 @@ using System.IO;
 using System.Linq;
 using System.Text;
 
+using LibDat.Data;
+
 namespace LibDat
 {
 	/// <summary>
@@ -12,30 +14,35 @@ namespace LibDat
 	/// </summary>
 	public class DatContainer
 	{
-		/// <summary>
-		/// Mapping of all known strings and other data found in the data section. Key = offset with respect to beginning of data section.
-		/// </summary>
-		public Dictionary<int, BaseData> DataEntries = new Dictionary<int, BaseData>();
-		/// <summary>
-		/// List of properties for the specified dat type. See code in Files directory.
-		/// </summary>
-		public List<BaseDat> Entries;
+        /// <summary>
+        /// Name of the dat file (without .dat extension)
+        /// </summary>
+        public readonly string DatName;
+
+        public readonly RecordInfo recordInfo;
+
+        /// <summary>
+        /// List of properties for the specified dat type. See code in Files directory.
+        /// </summary>
+        public List<Record> Records;
+
 		/// <summary>
 		/// Offset of the data section in the .dat file (Starts with 0xbbbbbbbbbbbbbbbb)
 		/// </summary>
 		public long DataTableBegin;
-		/// <summary>
-		/// Type of .dat file
-		/// </summary>
-		public Type DatType;
+
 		/// <summary>
 		/// Contains the entire unmodified data section of the .dat file
 		/// </summary>
 		private byte[] originalDataTable;
-		/// <summary>
-		/// Name of the dat file (without .dat extension)
-		/// </summary>
-		private readonly string datName;
+
+        /// <summary>
+        /// Mapping of all known strings and other data found in the data section. 
+        /// Key = offset with respect to beginning of data section.
+        /// 
+        /// </summary>
+        public Dictionary<int, AbstractData> DataEntries = new Dictionary<int, AbstractData>();
+
 
 
 		/// <summary>
@@ -45,8 +52,10 @@ namespace LibDat
 		/// <param name="fileName">Name of the dat file (with extension)</param>
 		public DatContainer(Stream inStream, string fileName)
 		{
-			datName = Path.GetFileNameWithoutExtension(fileName);
-			DatType = DatFactory.GetTypeName(datName);
+            DatName = Path.GetFileNameWithoutExtension(fileName);
+
+            // TODO
+            // recordInfo = ??????
 
 			using (BinaryReader br = new BinaryReader(inStream, Encoding.Unicode))
 			{
@@ -57,13 +66,12 @@ namespace LibDat
 		/// <summary>
 		/// Parses the .dat file found at path 'fileName'
 		/// </summary>
-		/// <param name="fileName">Path of .dat file to parse</param>
-		public DatContainer(string fileName)
+        /// <param name="filePath">Path of .dat file to parse</param>
+		public DatContainer(string filePath)
 		{
-			this.datName = Path.GetFileNameWithoutExtension(fileName);
-			DatType = DatFactory.GetTypeName(datName);
+            DatName = Path.GetFileNameWithoutExtension(filePath);
 
-			byte[] fileBytes = File.ReadAllBytes(fileName);
+			byte[] fileBytes = File.ReadAllBytes(filePath);
 
 			using (MemoryStream ms = new MemoryStream(fileBytes))
 			{
@@ -74,13 +82,61 @@ namespace LibDat
 			}
 		}
 
+        /// <summary>
+        /// Reads the .dat frile from the specified stream
+        /// </summary>
+        /// <param name="inStream">Stream containing contents of .dat file</param>
+        private void Read(BinaryReader inStream)
+        {
+            int numberOfEntries = inStream.ReadInt32();
+            if (inStream.ReadUInt64() == 0xBBbbBBbbBBbbBBbb)
+            {
+                Records = new List<Record>();
+                return;
+            }
+            inStream.BaseStream.Seek(-8, SeekOrigin.Current);
+
+            Records = new List<Record>(numberOfEntries);
+
+            if (DatName.Length == 0)
+            {
+                throw new Exception("Missing dat parser for file " + DatName);
+            }
+
+            for (int i = 0; i < numberOfEntries; i++)
+            {
+                Records.Add(RecordFactory.Create(DatName, inStream));
+            }
+
+            if (inStream.ReadUInt64() != 0xBBbbBBbbBBbbBBbb)
+            {
+                throw new ApplicationException("Missing magic number after records");
+            }
+
+            DataTableBegin = inStream.BaseStream.Position - 8;
+            inStream.BaseStream.Seek(-8, SeekOrigin.Current);
+            originalDataTable = inStream.ReadBytes((int)(inStream.BaseStream.Length - inStream.BaseStream.Position));
+
+            // Read all referenced string and data entries from the data following the entries (starting at magic number)
+            foreach (var item in Records)
+            {
+                AddDataToTable(item, inStream);
+            }
+        }
+
+
+        public object GetFieldValue(Record record, int index)
+        {
+            return record.GetField(index);
+        }
+
 		/// <summary>
 		/// Finds all known references to strings and other data in the data section and adds them to the DataEntries. 
 		/// Accomplished by reading the [StringIndex] and [DataIndex] attributes of our dat structure.
 		/// </summary>
 		/// <param name="entry">Dat parser created from parsing a single entry of the .dat file.</param>
 		/// <param name="inStream">Stream containing contents of .dat file. Stream position not preserved.</param>
-		private void AddDataToTable(BaseDat entry, BinaryReader inStream)
+		private void AddDataToTable(Record entry, BinaryReader inStream)
 		{
 			var properties = entry.GetType().GetProperties();
 
@@ -124,75 +180,9 @@ namespace LibDat
 			}
 		}
 
-		/// <summary>
-		/// Updates references to modified strings/data in the data section for the specified entry.
-		/// </summary>
-		/// <param name="entry">Entry being updated</param>
-		/// <param name="updatedOffsets">Mapping of all changed offsets. Key = original offset, Value = new offset.</param>
-		private void UpdateDataOffsets(BaseDat entry, Dictionary<long, long> updatedOffsets)
-		{
-			var properties = entry.GetType().GetProperties();
-			foreach (var prop in properties)
-			{
-				object[] customAttributes = prop.GetCustomAttributes(false);
 
-				if (customAttributes.Length == 0)
-					continue;
 
-				if (customAttributes.Any(n => n is UInt64Index) || customAttributes.Any(n => n is UInt32Index) ||
-					customAttributes.Any(n => n is Int32Index))
-					continue;
 
-				int offset = (int)prop.GetValue(entry, null);
-				if (updatedOffsets.ContainsKey(offset))
-				{
-					//Console.WriteLine("Updating offset {0} for {1} (now {2})", offset, prop.Name, updatedOffsets[offset]);
-					prop.SetValue(entry, (int)updatedOffsets[offset], null);
-				}
-			}
-		}
-
-		/// <summary>
-		/// Reads the .dat frile from the specified stream
-		/// </summary>
-		/// <param name="inStream">Stream containing contents of .dat file</param>
-		private void Read(BinaryReader inStream)
-		{
-			int numberOfEntries = inStream.ReadInt32();
-			if (inStream.ReadUInt64() == 0xBBbbBBbbBBbbBBbb)
-			{
-				Entries = new List<BaseDat>();
-				return;
-			}
-			inStream.BaseStream.Seek(-8, SeekOrigin.Current);
-
-			Entries = new List<BaseDat>(numberOfEntries);
-
-			if (DatType == null)
-			{
-				throw new Exception("Missing dat parser for type " + datName);
-			}
-
-			for (int i = 0; i < numberOfEntries; i++)
-			{
-				Entries.Add(DatFactory.Create(datName, inStream));
-			}
-
-			if (inStream.ReadUInt64() != 0xBBbbBBbbBBbbBBbb)
-			{
-				throw new ApplicationException("Missing magic number after records");
-			}
-
-			DataTableBegin = inStream.BaseStream.Position - 8;
-			inStream.BaseStream.Seek(-8, SeekOrigin.Current);
-			originalDataTable = inStream.ReadBytes((int)(inStream.BaseStream.Length - inStream.BaseStream.Position));
-
-			// Read all referenced string and data entries from the data following the entries (starting at magic number)
-			foreach (var item in Entries)
-			{
-				AddDataToTable(item, inStream);
-			}
-		}
 
 		public byte[] GetBytes()
 		{
@@ -205,10 +195,10 @@ namespace LibDat
 		/// <summary>
 		/// Saves parsed data to specified path
 		/// </summary>
-		/// <param name="fileName">Path to write contents to</param>
-		public void Save(string fileName)
+		/// <param name="filePath">Path to write contents to</param>
+		public void Save(string filePath)
 		{
-			using (FileStream outStream = File.Open(fileName, FileMode.Create))
+			using (FileStream outStream = File.Open(filePath, FileMode.Create))
 			{
 				Save(outStream);
 			}
@@ -224,12 +214,12 @@ namespace LibDat
 			Dictionary<long, long> changedOffsets = new Dictionary<long, long>();
 
 			BinaryWriter outStream = new BinaryWriter(rawOutStream, System.Text.Encoding.Unicode);
-			outStream.Write(Entries.Count);
+			outStream.Write(Records.Count);
 
-			if (Entries.Count > 0)
+			if (Records.Count > 0)
 			{
 				// Pretty ugly way to zero out the for sizeof(Entry) * EntryCount bytes
-				outStream.Write(new byte[(Entries[0] as BaseDat).GetSize() * Entries.Count]);
+				outStream.Write(new byte[recordInfo.Length * Records.Count]);
 			}
 
 			int newStartOfDataSection = (int)outStream.BaseStream.Position;
@@ -252,12 +242,40 @@ namespace LibDat
 			outStream.BaseStream.Seek(4, SeekOrigin.Begin);
 
 			// Now we must go through each StringIndex and DataIndex and update the index then save it
-			foreach (var item in Entries)
+			foreach (var item in Records)
 			{
 				UpdateDataOffsets(item, changedOffsets);
 				item.Save(outStream);
 			}
 		}
+
+        /// <summary>
+        /// Updates references to modified strings/data in the data section for the specified entry.
+        /// </summary>
+        /// <param name="entry">Entry being updated</param>
+        /// <param name="updatedOffsets">Mapping of all changed offsets. Key = original offset, Value = new offset.</param>
+        private void UpdateDataOffsets(Record entry, Dictionary<long, long> updatedOffsets)
+        {
+            var properties = entry.GetType().GetProperties();
+            foreach (var prop in properties)
+            {
+                object[] customAttributes = prop.GetCustomAttributes(false);
+
+                if (customAttributes.Length == 0)
+                    continue;
+
+                if (customAttributes.Any(n => n is UInt64Index) || customAttributes.Any(n => n is UInt32Index) ||
+                    customAttributes.Any(n => n is Int32Index))
+                    continue;
+
+                int offset = (int)prop.GetValue(entry, null);
+                if (updatedOffsets.ContainsKey(offset))
+                {
+                    //Console.WriteLine("Updating offset {0} for {1} (now {2})", offset, prop.Name, updatedOffsets[offset]);
+                    prop.SetValue(entry, (int)updatedOffsets[offset], null);
+                }
+            }
+        }
 
 		/// <summary>
 		/// Returns a CSV table with the contents of this dat container.
@@ -269,7 +287,7 @@ namespace LibDat
 			StringBuilder sb = new StringBuilder();
 
 			bool displayedHeader = false;
-			foreach (var item in Entries)
+			foreach (var item in Records)
 			{
 				var properties = item.GetType().GetProperties();
 
@@ -315,5 +333,7 @@ namespace LibDat
 
 			return sb.ToString();
 		}
-	}
+
+
+    }
 }
---
+++ LibDat/RecordFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace LibDat
+{
+	public static class RecordFactory
+	{
+
+        // TODO this property should be initialized on application start from external XML file
+        private static Dictionary<String, Object> parsers;
+
+        public static bool HasParser(string fileName)
+        {
+            if (fileName.EndsWith(".dat") ) // is it necessary ??
+            {
+                fileName = Path.GetFileNameWithoutExtension(fileName);
+            }
+
+            return parsers.ContainsKey(fileName);
+        }
+
+		public static Record Create(string fileName, BinaryReader inStream)
+		{
+            // 1. check for existence of XML description of parser for file 'fileName.dat'
+            // throw new Exception("Missing dat parser for type " + fileName);
+
+            // 2. Read and save 1 record from inStream
+
+            // TODO This is stub, delete later
+            Type t = Type.GetType(string.Format("LibDat.Files.{0}, LibDat", fileName));
+            return (Record)Activator.CreateInstance(t, new object[] { inStream });
+		}
+	}
+}
