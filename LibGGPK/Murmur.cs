51a03a7481a99e5d9d46de77d4a482c1c65bd958
MuxaJIbI4 <samonon@gmail.com>
2014-09-10 18:49:22 +0400

    reformatted and cleand up Murmur

--- LibGGPK/Murmur.cs
+++ LibGGPK/Murmur.cs
@@ -17,78 +17,76 @@ using System.Text;
 
 namespace LibGGPK
 {
-	class Murmur
-	{
-		private const uint DefaultMurmurSeed = 0;
+    static class Murmur
+    {
+        private const uint DefaultMurmurSeed = 0;
 
-		public static uint Hash2(string str, uint seed = DefaultMurmurSeed)
-		{
-			return Hash2(Encoding.Unicode.GetBytes(str.ToLower()), DefaultMurmurSeed);
-		}
+        public static uint Hash2(string str, uint seed = DefaultMurmurSeed)
+        {
+            return Hash2(Encoding.Unicode.GetBytes(str.ToLower()), DefaultMurmurSeed);
+        }
 
-		private static uint Hash2(byte[] data, uint seed = DefaultMurmurSeed)
-		{
-			// 'm' and 'r' are mixing constants generated offline.
-			// They're not really 'magic', they just happen to work well.
+        private static uint Hash2(byte[] data, uint seed = DefaultMurmurSeed)
+        {
+            // 'm' and 'r' are mixing constants generated offline.
+            // They're not really 'magic', they just happen to work well.
 
-			const uint m = 0x5bd1e995;
-			const int r = 24;
+            const uint m = 0x5bd1e995;
+            const int r = 24;
 
+            // Initialize the hash to a 'random' value
 
-			// Initialize the hash to a 'random' value
+            uint h = seed ^ (uint)data.Length;
 
-			uint h = seed ^ (uint)data.Length;
+            // Mix 4 bytes at a time into the hash
 
-			// Mix 4 bytes at a time into the hash
+            int dataIndex = 0;
+            int len = data.Length;
 
-			int dataIndex = 0;
-			int len = data.Length;
+            while (len >= 4)
+            {
+                uint k = BitConverter.ToUInt32(data, dataIndex);
 
-			while(len >= 4)
-			{
-				uint k = BitConverter.ToUInt32(data, dataIndex);
+                k *= m;
+                k ^= k >> r;
+                k *= m;
 
-				k *= m; 
-				k ^= k >> r; 
-				k *= m; 
-		
-				h *= m; 
-				h ^= k;
+                h *= m;
+                h ^= k;
 
-				dataIndex += 4;
-				len -= 4;
-			}
-	
-			// Handle the last few bytes of the input array
-			switch(len)
-			{
-				case 3:
-				{
-					h ^= (uint)data[dataIndex + 2] << 16;
-					goto case 2;
-				}
-				case 2:
-				{
-					h ^= (uint) data[dataIndex + 1] << 8;
-					goto case 1;
-				}
-				case 1:
-				{
-					h ^= (uint) data[dataIndex + 0];
-					h *= m;
-					break;
-				}
-			}
+                dataIndex += 4;
+                len -= 4;
+            }
 
-			// Do a few final mixes of the hash to ensure the last few
-			// bytes are well-incorporated.
+            // Handle the last few bytes of the input array
+            switch (len)
+            {
+                case 3:
+                    {
+                        h ^= (uint)data[dataIndex + 2] << 16;
+                        goto case 2;
+                    }
+                case 2:
+                    {
+                        h ^= (uint)data[dataIndex + 1] << 8;
+                        goto case 1;
+                    }
+                case 1:
+                    {
+                        h ^= (uint)data[dataIndex + 0];
+                        h *= m;
+                        break;
+                    }
+            }
 
-			h ^= h >> 13;
-			h *= m;
-			h ^= h >> 15;
+            // Do a few final mixes of the hash to ensure the last few
+            // bytes are well-incorporated.
 
-			return h;
-		} 
+            h ^= h >> 13;
+            h *= m;
+            h ^= h >> 15;
 
-	}
+            return h;
+        }
+    }
 }
